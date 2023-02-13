a0bcf1e87a2ec83bf54de15da9a50a8f6daed782
nandinihirpara <119646455+nandinihirpara@users.noreply.github.com>
2023-02-13 10:18:19 +0530

    Add files via upload

---
+++ prectice.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int u, t;
+    printf("Enter the number=");
+    scanf("%d", &u);
+    t = 2 * u - 1;
+    for (int i = 1; i <= t; i++)
+    {
+        for (int j = 1; j <= t; j++)
+        {
+            if (i == j || j == (t - i + 1))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
